768bad31e7a99d713dd1a4af41cdf78c1a9483e5
codershani <codershani13@gmail.com>
2024-09-05 12:32:39 +0530

    Added get flight api

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -5,14 +5,17 @@ const { FlightMiddlewares } = require("../../middlewares");
 
 const router = express.Router();
 
-// /api/v1/airports POST
+// /api/v1/flights POST
 router.post(
   "/",
   FlightMiddlewares.validateCreateRequest,
   FlightController.createFlight
 );
 
-// /api/v1/airports?trips=MUM-DEL GET
+// /api/v1/flights?trips=MUM-DEL GET
 router.get("/", FlightController.getAllFlights);
+
+// /api/v1/flights
+router.get("/:id", FlightController.getFlight);
 
 module.exports = router;
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -77,7 +77,23 @@ async function getAllFlights(query) {
   }
 }
 
+async function getFlight(id) {
+  try {
+    const flight = await flightRepository.get(id);
+    return flight;
+  } catch (error) {
+    if ((error.statusCode = StatusCodes.NOT_FOUND)) {
+      throw new AppError("Flight not found.", error.statusCode);
+    }
+    throw new AppError(
+      "Cannot fetch data of a flight.",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
 module.exports = {
   createFlight,
   getAllFlights,
+  getFlight,
 };
